deccc182a4438cfc2d5fb4090d663afb28cb03b6
Janu Prasad K <janu@hatio.co>
2017-11-15 17:04:45 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
 /**
- * @providesModule UPIDirectConnect
+ * @providesModule UPIDirectConnect2
  */
 
 'use strict';
 import React from 'react';
-import { AlertIOS, Platform, NativeModules } from 'react-native';
+import {  Platform, NativeModules } from 'react-native';
 
 const UPIDirectConnect = NativeModules.UPIDirectConnect;
 
-const Buttons = {
-    POSITIVE_BUTTON: "POSITIVE_BUTTON",
-    NEGATIVE_BUTTON: "NEGATIVE_BUTTON",
-    NEUTRAL_BUTTON: "NEUTRAL_BUTTON",
+const UPIDirect = {
+  initPayment(title) {
+     UPIDirectConnect.initPayment((title||null), null);
+  }
+    // alert(title, text, buttonConfig) {
+    //     let _buttonConfig = (buttonConfig||[{ type: UPIDirect.POSITIVE_BUTTON, text: 'OK', }]);
+    //     if (Platform.OS === 'ios') {
+    //         for(let j=0; j<_buttonConfig.length; j++) {
+    //             if("type" && _buttonConfig[j]) delete _buttonConfig[j]['type'];
+    //         }
+    //         AlertIOS.alert.apply(AlertIOS, [title, text, _buttonConfig]);
+    //     } else {
+    //         let _masterCallback = (buttonType) => {
+    //             for(let j=0; j<_buttonConfig.length; j++) {
+    //                 if("type" && _buttonConfig[j] && _buttonConfig[j].type === buttonType && ("onPress" in _buttonConfig[j] && typeof _buttonConfig[j].onPress == "function")) {
+    //                     _buttonConfig[j].onPress.apply(null, [{type: buttonType}]);
+    //                 }
+    //             }
+    //         };
+    //
+    //         SimpleAlertAndroid.alert((title||null), (text||null), _buttonConfig, _masterCallback);
+    //     }
+    // }
+
 };
 
-if (Platform.OS !== 'ios') {
-    Buttons = {
-        POSITIVE_BUTTON: UPIDirectConnect.POSITIVE_BUTTON,
-        NEGATIVE_BUTTON: UPIDirectConnect.NEGATIVE_BUTTON,
-        NEUTRAL_BUTTON: UPIDirectConnect.NEUTRAL_BUTTON,
-    };
-}
-
-const UPIDirectConnect = {
-    [Buttons.POSITIVE_BUTTON]: Buttons.POSITIVE_BUTTON,
-    [Buttons.NEGATIVE_BUTTON]: Buttons.NEGATIVE_BUTTON,
-    [Buttons.NEUTRAL_BUTTON]: Buttons.NEUTRAL_BUTTON,
-    alert(title, text, buttonConfig) {
-        let _buttonConfig = (buttonConfig||[{ type: UPIDirectConnect.POSITIVE_BUTTON, text: 'OK', }]);
-
-            let _masterCallback = (buttonType) => {
-                for(let j=0; j<_buttonConfig.length; j++) {
-                    if("type" && _buttonConfig[j] && _buttonConfig[j].type === buttonType && ("onPress" in _buttonConfig[j] && typeof _buttonConfig[j].onPress == "function")) {
-                        _buttonConfig[j].onPress.apply(null, [{type: buttonType}]);
-                    }
-                }
-            };
-
-            UPIDirectConnect.alert((title||null), (text||null), _buttonConfig, _masterCallback);
-
-    }
-    initPayment(params) {
-        UPIDirectConnect.initPayment((params||null), _masterCallback);
-    }
-};
-
-module.exports = UPIDirectConnect;
+module.exports = UPIDirect;
